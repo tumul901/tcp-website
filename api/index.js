1abcf1ec21c9af367240c19da1190d1d6d7fb7ff
tumul901 <tumshukla99@gmail.com>
2025-12-04 14:28:02 +0530

    fixing vercel deployment issue

--- api/index.js
+++
@@ -1,4 +0,0 @@
-// import app from '../backend/src/app.js';
-import app from '../backend/src/server.js'
-
-export default app;
--- api/test.js
+++
@@ -1,4 +0,0 @@
-// Simple test to verify the function works
-export default function handler(req, res) {
-  res.status(200).json({ message: 'API is working', path: req.url });
-}
